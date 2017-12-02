07ab2883ec7bd55e4c8fcfe1000615692470b7b7
Alexander Hughes <alehugh@nuevaschool.org>
2017-12-02 14:11:37 -0800

    modified runner.py a bit and made first training set (based on random functions)

--- runner.py
+++ runner.py
@@ -45,7 +45,8 @@ class Arena():
 				print "---"
 				print "Round " + str(__ + 1)
 				bet = input("Place your bet (-1 or 1)\n")
-				ai_bet = ai_func(zip(dice.h1, dice.h2), choice_history)
+				ai_bet = ai_func(zip(dice.h1, dice.h2)[-5:], choice_history[-5:]) # passes most recent 5
+				# NOTE: if the history is less than five, it passes a shorter list. 
 				print "AI bet: " + str(ai_bet)
 				roll = dice.roll()
 				print "Roll in format (your roll, ai roll)"
@@ -62,6 +63,47 @@ class Arena():
 			print "Winnings at end of match: " + str(pot)
 			persistant += pot
 		print "Total winnings: " + str(persistant)
+
+# Here's a runner class that will play nice with libgen to make training sets
+class Generator():
+	def __init__(self, player1func, player2func, rounds_per_match, history_length):
+		self.player1func = player1func
+		self.player2func = player2func
+		self.rounds_per_match = rounds_per_match
+		self.history_length = history_length
+	def run(self):
+		d = Dice()
+		choice_history = []
+		select_loc = random.randint(1, self.rounds_per_match)
+		for _ in range(select_loc):
+			p1_bet = self.player1func(zip(d.h1, d.h2)[-self.history_length:], choice_history[-self.history_length:])
+			p2_bet = self.player2func(zip(d.h1, d.h2)[-self.history_length:], choice_history[-self.history_length:])
+			roll = d.roll()
+			choice_history.append((p1_bet, p2_bet))
+		"""
+		print len(choice_history)
+		print select_loc
+		print d.h1
+		print d.h2
+		print choice_history
+		"""
+		h1_final = d.h1.pop()
+		h2_final = d.h2.pop()
+		other_choice = choice_history.pop()[1]
+		# following 'ideal choice' specification can be tweaked to reflect what we want the network to do
+		if other_choice == h1_final or other_choice == h2_final:
+			ideal_choice = other_choice
+		else:
+			ideal_choice = 0
+		"""
+		print ideal_choice
+		print "The following is being sent off"
+		print zip(d.h1, d.h2)[-self.history_length:]
+		print choice_history[-self.history_length:]
+		"""
+		# returns a number from -1 to 1 (currently -1, 0, or 1). To rescale for use in training, add 1 and divide by 2
+		# also returns last self.history_length dice histories and choice histories. Take care to buffer if no. iterations < 5
+		return zip(d.h1, d.h2)[-self.history_length:], choice_history[-self.history_length:], ideal_choice
 
 
 
